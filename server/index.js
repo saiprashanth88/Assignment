a1d5c3d900489addbcf620ae238f0b7c96d1404b
saiprashanth88 <saiprashanth817@gmail.com>
2024-06-04 16:18:05 +0530

    .

--- server/index.js
+++ server/index.js
@@ -19,14 +19,19 @@ app.use(express.json());
 
 //     }
 // ));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://assignment-gic2.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
-
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://assignment-gic2.vercel.app');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
+app.use(cors({
+    origin: 'https://assignment-gic2.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include all the methods your API supports
+    allowedHeaders: ['Content-Type', 'Authorization'], // Include any additional headers your API expects
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.) to be sent with the requests
+}));
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
