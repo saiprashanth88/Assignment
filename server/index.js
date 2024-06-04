c76290d95bf31b909fcf653ab4ccb704639b977f
saiprashanth88 <saiprashanth817@gmail.com>
2024-06-04 16:05:56 +0530

    .

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ connection();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://backend-tawny-eta.vercel.app/"],
+        origin: ["https://assignment-gic2.vercel.app/login"],
         methods: ["POST","GET"],
         credentials: true
 
